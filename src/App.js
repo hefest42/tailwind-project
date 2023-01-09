2cee4eb7cd65f27fa0f34cb9297445a1358db9b8
Nemanja Vidovic <hefest93@yahoo.com>
2023-01-09 16:28:44 +0100

    moved files around

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import SearchBar from "./components/SearchBar";
+import SearchBar from "./components/Search-Bar/SearchBar";
 
 function App() {
     return (
--- src/components/Search-Bar/SearchBar.js
+++ src/components/Search-Bar/SearchBar.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import BookmarkDropdownMenu from "./BookmarkDropdownMenu";
+
 import { BsBookmark } from "react-icons/bs";
 
 const SearchBar = () => {
@@ -22,13 +24,14 @@ const SearchBar = () => {
                 </form>
                 <button className="w-4/12 bg-bright-red hover:bg-dark-red text-dark-yellow rounded-r-md">Search</button>
             </div>
-            <div className="w-auto px-3 md:px-5 xl:px-10 h-full bg-lime-500 ">
-                <div className="h-full flex justify-center items-center cursor-pointer">
+            <div className="relative w-auto px-3 md:px-5 xl:px-10 h-full bg-lime-500 ">
+                <div className=" h-full flex justify-center items-center cursor-pointer">
                     <div className="hidden lg:block mr-4 text-lg text-dark-yellow">BOOKMARKS</div>
                     <div>
                         <BsBookmark className="text-dark-yellow w-6 h-6 md:w-8 md:h-8" />
                     </div>
                 </div>
+                <BookmarkDropdownMenu />
             </div>
         </div>
     );
