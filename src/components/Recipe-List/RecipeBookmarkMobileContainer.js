cc9007c83970d105beb21d1beb2506d00c8af5f6
Nemanja Vidovic <hefest93@yahoo.com>
2023-01-12 14:52:28 +0100

    added screen swiping

--- src/components/Recipe-List/RecipeBookmarkMobileContainer.js
+++ src/components/Recipe-List/RecipeBookmarkMobileContainer.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useSwipeable } from "react-swipeable";
 
 import RecipeListMobile from "./RecipeListMobile";
 import BookmarksMobile from "../Bookmarks/BookmarksMobile";
@@ -7,8 +8,25 @@ const RecipeBookmarkMobileContainer = () => {
     const [displaySearchResults, setDisplaySearchResults] = useState(false);
     const [displayBookmarks, setDisplayBookmarks] = useState(false);
 
+    const handlers = useSwipeable({
+        onSwipedLeft: () => {
+            setDisplaySearchResults(true);
+            setDisplayBookmarks(false);
+        },
+        onSwipedRight: () => {
+            setDisplaySearchResults(false);
+            setDisplayBookmarks(true);
+        },
+        swipeDuration: 500,
+        preventScrollOnSwipe: true,
+        trackMouse: true,
+    });
+
     return (
-        <div className="md:hidden absolute top-0 left-0 bg-dark-blue w-[60%] h-full mt-4 flex flex-col justify-start items-start overflow-y-scroll">
+        <div
+            {...handlers}
+            className="md:hidden absolute top-0 left-0 bg-dark-blue w-[60%] h-full mt-4 flex flex-col justify-start items-start overflow-y-scroll"
+        >
             <div className="flex w-full text-dark-yellow ">
                 <button
                     className="flex-1 outline-none bg-bright-red h-10"
