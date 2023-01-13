459ec661b35be13b4265c970c22131194fc2486f
Nemanja Vidovic <hefest93@yahoo.com>
2023-01-13 16:48:45 +0100

    positioning fixed

--- src/components/Desktop/Search-Bar/SearchBar.js
+++ src/components/Desktop/Search-Bar/SearchBar.js
@@ -8,8 +8,8 @@ const SearchBar = () => {
     const [displayBookmarks, setDisplayBookmarks] = useState(false);
 
     return (
-        <div className="w-full h-10 text-dark-yellow text-base lg:text-lg flex flex-row justify-between items-center">
-            <div className="h-full pr-6 flex justify-start items-center">RecipeSearch</div>
+        <div className="w-full h-10 text-dark-yellow text-lg lg:text-xl flex flex-row justify-between items-center">
+            <div className="h-full pr-10 flex justify-start items-center">RecipeSearch</div>
 
             <div className="h-full flex-1 flex bg-dark-orange">
                 <input type="text" placeholder="Search for Meals..." className="flex-1 outline-none" />
@@ -17,8 +17,8 @@ const SearchBar = () => {
                 <button className="w-28 h-full bg-bright-red">SEARCH</button>
             </div>
 
-            <div className="relative w-auto px-4 lg:pl-8 lg:w-[20%] h-full flex justify-center lg:justify-end items-center">
-                <p className="hidden lg:block mr-4">BOOKMARKS</p>
+            <div className="relative w-auto pl-10 lg:w-[20%] h-full flex justify-end items-center">
+                <p className="hidden lg:block mr-4">Bookmarks</p>
                 <div>
                     <BsBookmark className="w-8 h-8" />
                 </div>
